9db8c54ac6aafb2935b6d11c2fe97b59ca16dd86
floppa <jbarek2027@gmail.com>
2025-02-06 11:00:08 -0800

    cammands

--- main.py
+++ main.py
@@ -14,16 +14,28 @@ client = discord.Client(intents=discord.Intents.all())
 
 @client.event
 async def on_ready():
-  print("I'm in")
-  print(client.user)
+    print("I'm in")
+    print(client.user)
 
 @client.event
 async def on_message(message):
-  if message.channel.name == "drebees-bot":
-    if message.author != client.user:
-      user_name = message.author.display_name
-      if my_bot.should_i_respond(message.content, user_name):
-        response = my_bot.respond(message.content, user_name)
-        await message.channel.send(response)
+    try:
+        print(f"Received message: {message.content}")
+        
+        if message.author.display_name == ".i.am.floppa._.":
+            print("Message from .i.am.floppa._.")
+            await message.channel.send("Hello, .i.am.floppa._.! How can I assist you today?")
+        
+        if message.channel.name == "james_bot":  
+            if message.author != client.user: 
+                user_name = message.author.display_name
+                if my_bot.should_i_respond(message.content, user_name):
+                    response = my_bot.respond(message.content, user_name)
+                    await message.channel.send(response)
+    except Exception as e:
+        print(f"Error: {e}")
 
-client.run(my_secret)
+if my_secret:
+    client.run(my_secret)
+else:
+    print("Error: DISCORD_BOT_TOKEN environment variable not set.")

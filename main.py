472457d66f8030c032c9d5ef1df7494399a9b966
floppa <jbarek2027@gmail.com>
2025-05-21 11:49:38 -0700

    s

--- main.py
+++ main.py
@@ -22,9 +22,9 @@ async def on_message(message):
     try:
         print(f"Received message: {message.content}")
         
-        if message.author.display_name == ".i.am.floppa._.":
-            print("Message from .i.am.floppa._.")
-            await message.channel.send("Hello, .i.am.floppa._.! How can I assist you today?")
+        if message.author.display_name == "sir.duck.alot":
+            print("Message from sir.duck.alot")
+            await message.channel.send("Hello, sir.duck.alot! How can I assist you today?")
         
         if message.channel.name == "james_bot":  
             if message.author != client.user: 
